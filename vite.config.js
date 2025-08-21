82b77c2a20506ae7647c2f70ca3ab83e3ed86c46
[Lakshya Bhati] <[lakshyabhati0071@gmail.com]>
2025-08-21 19:36:21 +0530

    update more

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
-   base: '/fortune.com/',
+   base: '/fcs.com',
   plugins: [
     react(),
     viteImagemin({
